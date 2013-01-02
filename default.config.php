58a58a0ea37664f45d1335641fa26d9b66a8943c
Alexis Algoud <alexis@alexis-pc.(none)>
2013-01-02 16:16:10 +0100

    config

---
+++ default.config.php
@@ -0,0 +1,23 @@
+<?php
+
+	define('ROOT','/var/www/ATM/dolibarr/');
+	define('COREROOT','/var/www/ATM/atm-core/');
+
+	define('HTTP','http://127.0.0.1/ATM/dolibarr/');
+
+	if(defined('INC_FROM_CRON_SCRIPT')) {
+		include(ROOT."master.inc.php");	
+	}
+	else {
+		include(ROOT."main.inc.php");
+	}
+
+	define('DB_HOST',$dolibarr_main_db_host);
+	define('DB_NAME',$dolibarr_main_db_name);
+	define('DB_USER',$dolibarr_main_db_user);
+	define('DB_PASS',$dolibarr_main_db_pass);
+	define('DB_DRIVER','mysqli');
+
+	require(COREROOT.'inc.core.php');
+	
+	
