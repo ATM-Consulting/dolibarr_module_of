9c5341a8bacc573700294016e595e42fe73ffa7f
Maxime Kohlhaas <maxime@atm-consulting.fr>
2014-11-21 09:11:06 +0100

    Debug

--- liste.php
+++ liste.php
@@ -115,7 +115,7 @@ global $langs,$db,$user,$ASSET_LINK_ON_FIELD,$conf;
 	
 	$r->liste($ATMdb, $sql, array(
 		'limit'=>array(
-			'nbLine'=>$conf->global->MAIN_SIZE_LISTE_LIMIT
+			'nbLine'=>$conf->liste_limit
 		)
 		,'subQuery'=>array()
 		,'link'=>array(
