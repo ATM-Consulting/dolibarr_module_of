0d4b01b3120d5a8a726f0b0418ffbfd8b7305537
phf <phf@atm-consulting.fr>
2015-03-02 16:23:02 +0100

    [CORE] FIX : la modification du statut d'un OF n'est maintenant disponible que sur le mode view (#evo).

--- tpl/fiche_of.tpl.php
+++ tpl/fiche_of.tpl.php
@@ -42,9 +42,9 @@
 				<tr><td>Date de lancement</td><td>[assetOf.date_lancement;strconv=no]</td></tr>
 				<tr><td>Temps estimé de fabrication</td><td>[assetOf.temps_estime_fabrication;strconv=no] heure(s)</td></tr>
 				<tr><td>Temps réel de fabrication</td><td>[assetOf.temps_reel_fabrication;strconv=no] heure(s)</td></tr>
-				<tr><td>Statut</td><td><span style="display:none;">[assetOf.status;strconv=no]</span>[assetOf.statustxt;strconv=no],
-					[onshow;block=begin;when [view.status]!='CLOSE']
-						<span class="viewmode notinparentview">passer à l'état :
+				<tr><td>Statut</td><td><span style="display:none;">[assetOf.status;strconv=no]</span>[assetOf.statustxt;strconv=no]
+					[onshow;block=begin;when [view.status]!='CLOSE';when [view.mode]=='view']
+						<span class="viewmode notinparentview">, passer à l'état :
 						[onshow;block=begin;when [view.status]=='DRAFT']
 							<input type="button" onclick="if (confirm('Valider cet Ordre de Fabrication ?')) { submitForm([assetOf.id]); }" class="butAction" name="valider" value="Valider">
 						[onshow;block=end]
