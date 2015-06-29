78bd09a65cee75bf82eea169e6539e37b672f1d7
Alexis Algoud <alexis@atm-consulting.fr>
2015-06-29 15:33:49 +0200

    FIX sellist for OF

--- core/modules/modasset.class.php
+++ core/modules/modasset.class.php
@@ -511,7 +511,7 @@ class modAsset extends DolibarrModules
 		$res = $extrafields->addExtraField('type_asset', 'Type Equipement', 'sellist', 0, 255, 'product',0,0,'',$param);
 		
 		$extrafields=new ExtraFields($this->db);
-		$res = $extrafields->addExtraField('fk_of', 'Ordre de Fabrication', 'int', 0, '', 'projet_task');
+        $res = $extrafields->addExtraField('fk_of', 'Ordre de Fabrication', 'sellist', 0, '', 'projet_task',0,0,'',serialize(array('options'=>array('assetOf:numero:rowid'=>null))));
 
         define('INC_FROM_DOLIBARR',true);
         dol_include_once('/asset/config.php');
