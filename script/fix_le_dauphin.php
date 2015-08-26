77236eaf7e83543e982f27b2950efc571c26349d
geoffrey.girard <geoffre.girard@atm-consulting.fr>
2015-08-26 15:09:50 +0200

    ADD : update script spécifique le dauphin pour remettre à jour proprement tous les stocks

--- script/fix_le_dauphin.php
+++ script/fix_le_dauphin.php
@@ -4,9 +4,15 @@ define('INC_FROM_CRON_SCRIPT', true);
 set_time_limit(0);
 require('../config.php');
 
+dol_include_once('/product/class/product.class.php');
+dol_include_once('/asset/class/asset.class.php');
+
 $PDOdb=new TPDOdb;
 
-$sql = "SELECT aol.rowid, aol.lot_number FROM ".MAIN_DB_PREFIX."assetOf_line as aol WHERE 1";
+$user = new User($db);
+$user->fetch(1);
+
+/*$sql = "SELECT aol.rowid, aol.lot_number FROM ".MAIN_DB_PREFIX."assetOf_line as aol WHERE 1";
 $PDOdb->Execute($sql);
 $Tres = $PDOdb->Get_All();
 
@@ -20,5 +26,98 @@ foreach ($Tres as $res) {
 		$sql = "INSERT INTO ".MAIN_DB_PREFIX."element_element (fk_source, fk_target, sourcetype, targettype)
 				VALUES (".$res->rowid.",".$res2->rowid.",'TAssetOFLine','TAsset')";
 		$PDOdb->Execute($sql);
+	}
+}*/
+
+//On prends tous les produits finis pour les changer d'entrepôt
+
+if(GETPOST('TransfertStock')){
+	echo '<hr>CHANGEMENT D\'ENTREPOT DES PRODUITS FINIS<hr>';
+	
+	$sql = "SELECT p.rowid 
+			FROM ".MAIN_DB_PREFIX."product as p
+			WHERE p.finished = 1 AND stock > 0 AND fk_product_type = 0";
+	
+	$PDOdb->Execute($sql);
+	
+	while ($res = $PDOdb->Get_line()) {
+		$product = new Product($db);
+		$product->fetch($res->rowid);
+		$product->load_stock();
+		
+		// Remove stock
+		$result1=$product->correct_stock(
+			$user,
+			1,
+			$product->stock_reel,
+			1,
+			"Transfert de stock ".date('d/m/Y H:i'),
+			$product->stock_warehouse[1]->pmp
+		);
+	
+		if($result1) echo $product->label." Transfert de stock ".date('d/m/Y H:i')." ---> RETRAIT<br>";
+		else{
+			echo "ERREUR RETRAIT"; break;
+		} 
+		
+		// Add stock
+		$result2=$product->correct_stock(
+			$user,
+			2,
+			$product->stock_reel,
+			0,
+			"Transfert de stock ".date('d/m/Y H:i'),
+			$product->stock_warehouse[1]->pmp
+		);
+	
+		if($result2) echo $product->label." Transfert de stock ".date('d/m/Y H:i')." ---> AJOUT<br>";
+		else{
+			echo "ERREUR AJOUT"; break;
+		} 
+	}
+}
+
+if(GETPOST('ResetAsset')){
+	echo '<hr>REMISE A ZERO DES EQUIPEMENTS EN FONTION DES STOCK PRODUIT MATIERES PREMIERE<hr>';
+	
+	$sql = "SELECT p.rowid 
+			FROM ".MAIN_DB_PREFIX."product as p
+			WHERE p.finished = 0 AND stock > 0 AND fk_product_type = 0";
+
+	$PDOdb->Execute($sql);
+	$TProductIds = $PDOdb->Get_All();
+	
+	foreach ($TProductIds as $productid) {
+		
+		
+		$sql = "SELECT rowid, contenance_value, contenancereel_value FROM ".MAIN_DB_PREFIX."asset WHERE fk_product = ".$productid->rowid." ORDER BY date_cre ASC";
+		$PDOdb->Execute($sql);
+		$TAssetIds = $PDOdb->Get_All();
+		
+		$product = new Product($db);
+		$product->fetch($productid->rowid);
+		$product->load_stock();
+		
+		echo $product->id." ".$product->libelle.'<br>';
+		
+		$cpt = 1;
+		foreach ($TAssetIds as $row) {
+			
+			if(count($TAssetIds) != $cpt){
+				$cpt++;
+				$mouvementStock = new TAssetStock;
+				$mouvementStock->mouvement_stock($PDOdb, $user, $row->rowid, ($row->contenance_value - $row->contenancereel_value), 'RAZ des stocks Equipements', 1);
+
+				$PDOdb->Execute('UPDATE '.MAIN_DB_PREFIX.'asset SET contenancereel_value = 0 WHERE rowid = '.$row->rowid);
+			}
+			else{
+				$cpt++;
+				$mouvementStock = new TAssetStock;
+				$mouvementStock->mouvement_stock($PDOdb, $user, $row->rowid, ($product->stock_reel - $row->contenancereel_value), 'RAZ des stocks Equipements', 1);
+				
+				$PDOdb->Execute('UPDATE '.MAIN_DB_PREFIX.'asset SET contenancereel_value = '.$product->stock_reel.' WHERE rowid = '.$row->rowid);
+			}
+		}
+		
 	}
 }
