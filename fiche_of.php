7f4dcc179172c901b2a0e75ee9f61bc59dc6ff68
Gauthier <gauthier.verdol@atm-consulting.fr>
2015-09-07 18:22:11 +0200

    FIX : code barre

--- fiche_of.php
+++ fiche_of.php
@@ -532,10 +532,48 @@ function generateODTOF(&$PDOdb, &$assetOf) {
 
 }
 
+function drawCross($im, $color, $x, $y){
+	
+	imageline($im, $x - 10, $y, $x + 10, $y, $color);
+    imageline($im, $x, $y- 10, $x, $y + 10, $color);
+	
+}
+
 function getBarCodePicture(&$assetOf) {
 	
+	dol_include_once('/asset/php_barcode/php-barcode.php');
+	
 	$code = $assetOf->numero;
-	include_once './script/get_barcode_pic.php';
+	
+  	$fontSize = 10;   // GD1 in px ; GD2 in point
+  	$marge    = 10;   // between barcode and hri in pixel
+  	$x        = 145;  // barcode center
+  	$y        = 50;  // barcode center
+  	$height   = 50;   // barcode height in 1D ; module size in 2D
+  	$width    = 2;    // barcode height in 1D ; not use in 2D
+  	$angle    = 0;   // rotation in degrees : nb : non horizontable barcode might not be usable because of pixelisation
+  
+  	$type     = 'code128';
+
+ 	$im     = imagecreatetruecolor(300, 100);
+  	$black  = ImageColorAllocate($im,0x00,0x00,0x00);
+  	$white  = ImageColorAllocate($im,0xff,0xff,0xff);
+  	$red    = ImageColorAllocate($im,0xff,0x00,0x00);
+  	$blue   = ImageColorAllocate($im,0x00,0x00,0xff);
+  	imagefilledrectangle($im, 0, 0, 300, 300, $white);
+
+  	$data = Barcode::gd($im, $black, $x, $y, $angle, $type, array('code'=>$code), $width, $height);
+  	if ( isset($font) ){
+    	$box = imagettfbbox($fontSize, 0, $font, $data['hri']);
+		$len = $box[2] - $box[0];
+		Barcode::rotate(-$len / 2, ($data['height'] / 2) + $fontSize + $marge, $angle, $xt, $yt);
+		imagettftext($im, $fontSize, $angle, $x + $xt, $y + $yt, $blue, $font, $data['hri']);
+	}
+
+	$tmpfname = tempnam(sys_get_temp_dir(), 'barcode_pic');
+	imagepng($im, $tmpfname);
+	imagedestroy($im);
+	
 	return $tmpfname;
 	
 }
