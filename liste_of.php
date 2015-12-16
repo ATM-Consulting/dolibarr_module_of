c6a432b5f2ecd7a551f41205ad59ad0c3571cc6a
phf <phf@atm-consulting.fr>
2015-12-16 10:33:54 +0100

    REFACTORING

--- liste_of.php
+++ liste_of.php
@@ -1,8 +1,8 @@
 <?php
 	require('config.php');
-	require('./class/asset.class.php');
-	require('./class/ordre_fabrication_asset.class.php');
-	require_once DOL_DOCUMENT_ROOT.'/product/class/product.class.php';
+	dol_include_once('/asset/class/asset.class.php');
+	dol_include_once('asset/class/ordre_fabrication_asset.class.php');
+	dol_include_once('/product/class/product.class.php');
 	dol_include_once('/commande/class/commande.class.php');
 	
 	if(!$user->rights->asset->all->lire) accessforbidden();
@@ -12,70 +12,70 @@
 	dol_include_once("/core/lib/company.lib.php");
 	
 	$langs->load('asset@asset');
-	
+	$PDOdb = new TPDOdb;
 	$action = __get('action');
-
-	switch ($action) {
+	
+	switch ($action) 
+	{
 		case 'createOFCommande':
-
-			$ATMdb = new TPDOdb;
-
-			_createOFCommande($ATMdb, $_REQUEST['TProducts'], $_REQUEST['TQuantites'], $_REQUEST['fk_commande'], $_REQUEST['fk_soc'], isset($_REQUEST['subFormAlone']));
+			_createOFCommande($PDOdb, $_REQUEST['TProducts'], $_REQUEST['TQuantites'], $_REQUEST['fk_commande'], $_REQUEST['fk_soc'], isset($_REQUEST['subFormAlone']));
 			_liste();
 			break;
 		case 'printTicket':
-			_printTicket();
+			_printTicket($PDOdb);
 		default:
-			_liste();
+			_liste($PDOdb);
 			break;
 	}	
 	
-
-function _createOFCommande($ATMdb, $TProduct, $TQuantites, $fk_commande, $fk_soc, $oneOF = false) {
 /*
  * Créé des Of depuis un tableau de product
  */	
- 
+function _createOFCommande(&$PDOdb, $TProduct, $TQuantites, $fk_commande, $fk_soc, $oneOF = false) 
+{
 	global $db, $langs;
-
-	if(!empty($TProduct)) {
-			if($oneOF) {
+	
+	if(!empty($TProduct)) 
+	{
+		if($oneOF) 
+		{
+			$assetOf = new TAssetOF;
+			$assetOf->fk_commande = $fk_commande;
+		}
+		
+		foreach($TProduct as $fk_commandedet => $v) 
+		{
+			foreach($v as $fk_product=>$dummy) 
+			{
+				if(!$oneOF) 
+				{
 					$assetOf = new TAssetOF;
 					$assetOf->fk_commande = $fk_commande;
-			}
-			
-			foreach($_REQUEST['TProducts'] as $fk_commandedet=>$v) {
-				
-				foreach($v as $fk_product=>$dummy) {
-					if(!$oneOF) {
-							$assetOf = new TAssetOF;
-							$assetOf->fk_commande = $fk_commande;
-					}
-					
-					if($assetOf->fk_commande > 0) {
-						$com = new Commande($db);
-						$com->fetch($assetOf->fk_commande);
-						$assetOf->fk_project = $com->fk_project;
-						if(!empty($com->date_livraison)) $assetOf->date_besoin = $com->date_livraison;
-					}
-					
-					$assetOf->fk_soc = $fk_soc;
-					$assetOf->addLine($ATMdb, $fk_product, 'TO_MAKE', $TQuantites[$fk_commandedet], 0, '', 0, $fk_commandedet);
-					$assetOf->save($ATMdb);
-					
 				}
+				
+				if($assetOf->fk_commande > 0) 
+				{
+					$com = new Commande($db);
+					$com->fetch($assetOf->fk_commande);
+					$assetOf->fk_project = $com->fk_project;
+					if(!empty($com->date_livraison)) $assetOf->date_besoin = $com->date_livraison;
+				}
+				
+				$assetOf->fk_soc = $fk_soc;
+				$assetOf->addLine($PDOdb, $fk_product, 'TO_MAKE', $TQuantites[$fk_commandedet], 0, '', 0, $fk_commandedet);
+				$assetOf->save($PDOdb);
+				
 			}
-			
-			setEventMessage($langs->trans('OFAsset')." créé(s) avec succès", 'mesgs');
-			
+		}
+		
+		setEventMessage($langs->trans('OFAsset')." créé(s) avec succès", 'mesgs');
 	}
 			
 }
 
-function _liste() {
+function _liste(&$PDOdb)
+{
 	global $langs,$db,$user,$conf;
-	
-	$langs->load('asset@asset');
 	
 	llxHeader('',$langs->trans('ListOFAsset'),'','');
 	getStandartJS();
@@ -90,13 +90,9 @@ function _liste() {
 	$fk_product=__get('fk_product',0,'integer');
 	$fk_commande=__get('fk_commande',0,'integer');
 
-
-	if($fk_product > 0){
-		if(is_file(DOL_DOCUMENT_ROOT."/lib/product.lib.php")) require_once(DOL_DOCUMENT_ROOT."/lib/product.lib.php");
-		else require_once(DOL_DOCUMENT_ROOT."/core/lib/product.lib.php");
-		
-		//require_once(DOL_DOCUMENT_ROOT."/product/class/product.class.php");
-		dol_include_once("/product/class/product.class.php");
+	if($fk_product > 0)
+	{
+		dol_include_once('/core/lib/product.lib.php');
 
 		$product = new Product($db);
 		$result=$product->fetch($fk_product);	
@@ -105,25 +101,22 @@ function _liste() {
 		$titre=$langs->trans("CardProduct".$product->type);
 		$picto=($product->type==1?'service':'product');
 		dol_fiche_head($head, 'tabOF2', $titre, 0, $picto);
-
-	} elseif($fk_commande > 0) {
-		if(is_file(dol_buildpath("/lib/order.lib.php", 1))) dol_include_once("/lib/order.lib.php");
-		else dol_include_once("/core/lib/order.lib.php");		
-
-		dol_include_once("/commande/class/commande.class.php");
-
+	} 
+	elseif($fk_commande > 0) 
+	{
+		dol_include_once("/core/lib/order.lib.php");
+		
 		$commande = new Commande($db);
-		$result=$commande->fetch($fk_commande);	
-
+		$result=$commande->fetch($fk_commande);
+		
 		$head=commande_prepare_head($commande, $user);
 		$titre=$langs->trans("CustomerOrder".$product->type);
 		dol_fiche_head($head, 'tabOF3', $titre, 0, "order");
-
 	}
 	
 	$form=new TFormCore;
-	
 	$assetOf=new TAssetOF;
+	
 	$r = new TSSRenderControler($assetOf);
 
 	$sql="SELECT ofe.rowid, ofe.numero, ofe.fk_soc, s.nom as client, SUM(ofel.qty) as nb_product_to_make, ofel.fk_product, p.label as product, ofe.ordre, ofe.date_lancement , ofe.date_besoin
@@ -132,57 +125,43 @@ function _liste() {
 		  LEFT JOIN ".MAIN_DB_PREFIX."assetOf_line ofel ON (ofel.fk_assetOf=ofe.rowid AND ofel.type = 'TO_MAKE')
 		  LEFT JOIN ".MAIN_DB_PREFIX."product p ON p.rowid = ofel.fk_product
 		  LEFT JOIN ".MAIN_DB_PREFIX."societe s ON s.rowid = ofe.fk_soc
-		  WHERE ofe.entity=".$conf->entity
-		  ;
+		  WHERE ofe.entity=".$conf->entity;
 
-	if($fk_soc>0) {$sql.=" AND ofe.fk_soc=".$fk_soc; }
-	if($fk_product>0) {$sql.=" AND ofel.fk_product=".$fk_product;}
-	if($fk_commande>0) {$sql.=" AND ofe.fk_commande=".$fk_commande;}
-	//if(isset($_REQUEST['fk_product'])){$sql.=" AND e.fk_product=".$_REQUEST['fk_product']; $fk_product=$_REQUEST['fk_product'];}
+	if($fk_soc>0) $sql.=" AND ofe.fk_soc=".$fk_soc; 
+	if($fk_product>0) $sql.=" AND ofel.fk_product=".$fk_product;
+	if($fk_commande>0) $sql.=" AND ofe.fk_commande=".$fk_commande;
 	
 	$sql.=" GROUP BY ofe.rowid ";
 	
 	// TODO je me rappelle plus pourquoi j'ai fait cette merde mais ça fait planter le tri, donc à virer. 
-	
-	
 	if($conf->global->ASSET_OF_LIST_BY_ROWID_DESC) $orderBy['ofe.rowid']='DESC';
 	else $orderBy['ofe.date_cre']='DESC';
 	
-	/*if(isset($_REQUEST['fk_product'])) {
-		$sql.= ' AND ofel.fk_product='.$_REQUEST['fk_product'].' AND ofel.type = "TO_MAKE"';		
-	}*/
-	
 	$TMath=array();
 	$THide = array('rowid','fk_user','fk_product','fk_soc');
-	if(empty($user->rights->asset->of->price)){
-		 $THide[] = 'total_cost';
-	}
-	else {
-		$TMath['total_cost']='sum';
-	}
 	
-	if(!empty($_REQUEST['fk_product'])) $TMath['nb_product_to_make']='sum';
+	if(empty($user->rights->asset->of->price)) $THide[] = 'total_cost';
+	else $TMath['total_cost']='sum';
 	
+	if(!empty($fk_product)) $TMath['nb_product_to_make']='sum';
 	
 	$form=new TFormCore($_SERVER['PHP_SELF'], 'form', 'GET');
-
-	if ($fk_commande > 0) echo $form->hidden('fk_commande', $fk_commande);
-	echo $form->hidden('action', 'printTicket');
 	
-	$ATMdb=new TPDOdb;
-
+	echo $form->hidden('action', 'printTicket');
+	if ($fk_commande > 0) echo $form->hidden('fk_commande', $fk_commande);
 	if($fk_product > 0) echo $form->hidden('fk_product', $fk_product); // permet de garder le filtre produit quand on est sur l'onglet OF d'une fiche produit
-	$r->liste($ATMdb, $sql, array(
+	
+	$r->liste($PDOdb, $sql, array(
 		'limit'=>array(
 			'nbLine'=>$conf->liste_limit
 		)
 		,'orderBy'=>$orderBy
 		,'subQuery'=>array()
 		,'link'=>array(
-			'Utilisateur en charge'=>'<a href="'.DOL_URL_ROOT.'/user/card.php?id=@fk_user@">'.img_picto('','object_user.png','',0).' @val@</a>'
-			,'numero'=>'<a href="fiche_of.php?id=@rowid@">'.img_picto('','object_list.png','',0).' @val@</a>'
-			,'product'=>'<a href="'.DOL_URL_ROOT.'/product/card.php?id=@fk_product@">'.img_picto('','object_product.png','',0).' @val@</a>'
-			,'client'=>'<a href="'.DOL_URL_ROOT.'/societe/soc.php?id=@fk_soc@">'.img_picto('','object_company.png','',0).' @val@</a>'
+			'Utilisateur en charge'=>'<a href="'.dol_buildpath('/user/card.php?id=@fk_user@', 2).'">'.img_picto('','object_user.png','',0).' @val@</a>'
+			,'numero'=>'<a href="'.dol_buildpath('/asset/fiche_of.php?id=@rowid@"', 2).'>'.img_picto('','object_list.png','',0).' @val@</a>'
+			,'product'=>'<a href="'.dol_buildpath('/product/card.php?id=@fk_product@', 2).'">'.img_picto('','object_product.png','',0).' @val@</a>'
+			,'client'=>'<a href="'.dol_buildpath('/societe/soc.php?id=@fk_soc@', 2).'">'.img_picto('','object_company.png','',0).' @val@</a>'
 			,'printTicket'=>'<input style=width:40px;"" type="number" value="0" name="printTicket[@rowid@]" min="0" />'
 		)
 		,'translate'=>array()
@@ -200,7 +179,7 @@ function _liste() {
 			,'picto_precedent'=>img_picto('','back.png', '', 0)
 			,'picto_suivant'=>img_picto('','next.png', '', 0)
 			,'noheader'=> (int)isset($_REQUEST['fk_soc']) | (int)isset($_REQUEST['fk_product'])
-			,'messa geNothing'=>"Il n'y a aucun ".$langs->trans('OFAsset')." à afficher"
+			,'messageNothing'=>"Il n'y a aucun ".$langs->trans('OFAsset')." à afficher"
 			,'picto_search'=>img_picto('','search.png', '', 0)
 		)
 		,'title'=>array(
@@ -230,17 +209,16 @@ function _liste() {
         )
 	));
 	
-	echo '<p align="right"><input class="button" type="submit" name="print" value="Impression étiquette" /></p>';
+	echo '<p align="right"><input class="button" type="submit" name="print" value="'.$langs->trans('ofPrintTicket').'" /></p>';
 	
 	$form->end();
 	
-	// 
 	// On n'affiche pas le bouton de création d'OF si on est sur la liste OF depuis l'onglet "OF" de la fiche commande
-	if($fk_commande) {
-		
+	if($fk_commande) 
+	{
 		$commande=new Commande($db);
 		$commande->fetch($fk_commande);	
-				
+		
 		$r2 = new TSSRenderControler($assetOf);
 
 		$sql = "SELECT c.rowid as fk_commandedet, p.rowid as rowid, p.ref as refProd, p.label as nomProd, c.qty as qteCommandee";
@@ -304,20 +282,19 @@ function _liste() {
 		$db->free($resql);
 
 
-	} else {
-		                
-		if(!empty($_REQUEST['fk_product'])) {
-		    
-            
+	} 
+	else 
+	{
+		if(!empty($fk_product)) 
+		{
             $sql="SELECT ofe.rowid, ofe.numero, ofe.fk_soc, s.nom as client, SUM(IF(ofel.qty>0,ofel.qty,ofel.qty_needed) ) as nb_product_needed, ofel.fk_product, p.label as product, ofe.ordre, ofe.date_lancement , ofe.date_besoin
             , ofe.status, ofe.fk_user, ofe.total_cost
               FROM ".MAIN_DB_PREFIX."assetOf as ofe 
               LEFT JOIN ".MAIN_DB_PREFIX."assetOf_line ofel ON (ofel.fk_assetOf=ofe.rowid AND ofel.type = 'NEEDED')
               LEFT JOIN ".MAIN_DB_PREFIX."product p ON p.rowid = ofel.fk_product
               LEFT JOIN ".MAIN_DB_PREFIX."societe s ON s.rowid = ofe.fk_soc
-              WHERE ofe.entity=".$conf->entity." AND ofel.fk_product=".$_REQUEST['fk_product']." AND ofe.status!='CLOSE'";
-              ;
-
+              WHERE ofe.entity=".$conf->entity." AND ofel.fk_product=".$fk_product." AND ofe.status!='CLOSE'";
+			  
             $sql.=" GROUP BY ofe.rowid ";
             
             if($conf->global->ASSET_OF_LIST_BY_ROWID_DESC) $orderBy['ofe.rowid']='DESC';
@@ -325,28 +302,24 @@ function _liste() {
             
             $TMath=array();
             $THide = array('rowid','fk_user','fk_product','fk_soc');
-            if(empty($user->rights->asset->of->price)){
-                 $THide[] = 'total_cost';
-            }
-            else {
-                $TMath['total_cost']='sum';
-            }
+            if(empty($user->rights->asset->of->price)) $THide[] = 'total_cost';
+            else $TMath['total_cost']='sum';
             
-            if(!empty($_REQUEST['fk_product'])) $TMath['nb_product_needed']='sum';
+            $TMath['nb_product_needed']='sum';
             
             $l=new TListviewTBS('listeofproductneeded');
-            print '<strong>Liste OF ayant besoin du produit</strong>';
-            echo $l->render($ATMdb, $sql, array(
+            echo $langs->trans('ofListProductNeeded');
+            echo $l->render($PDOdb, $sql, array(
                 'limit'=>array(
                     'nbLine'=>$conf->liste_limit
                 )
                 ,'orderBy'=>$orderBy
                 ,'subQuery'=>array()
                 ,'link'=>array(
-                    'Utilisateur en charge'=>'<a href="'.DOL_URL_ROOT.'/user/card.php?id=@fk_user@">'.img_picto('','object_user.png','',0).' @val@</a>'
-                    ,'numero'=>'<a href="fiche_of.php?id=@rowid@">'.img_picto('','object_list.png','',0).' @val@</a>'
-                    ,'product'=>'<a href="'.DOL_URL_ROOT.'/product/card.php?id=@fk_product@">'.img_picto('','object_product.png','',0).' @val@</a>'
-                    ,'client'=>'<a href="'.DOL_URL_ROOT.'/societe/soc.php?id=@fk_soc@">'.img_picto('','object_company.png','',0).' @val@</a>'
+                    'Utilisateur en charge'=>'<a href="'.dol_buildpath('/user/card.php?id=@fk_user@', 2).'">'.img_picto('','object_user.png','',0).' @val@</a>'
+                    ,'numero'=>'<a href="'.dol_buildpath('/asset/fiche_of.php?id=@rowid@', 2).'">'.img_picto('','object_list.png','',0).' @val@</a>'
+                    ,'product'=>'<a href="'.dol_buildpath('/product/card.php?id=@fk_product@', 2).'">'.img_picto('','object_product.png','',0).' @val@</a>'
+                    ,'client'=>'<a href="'.dol_buildpath('/societe/soc.php?id=@fk_soc@', 2).'">'.img_picto('','object_company.png','',0).' @val@</a>'
                 )
                 ,'translate'=>array()
                 ,'hide'=>$THide
@@ -391,14 +364,14 @@ function _liste() {
 		
 		echo '<div class="tabsAction">';
 		
-		if ($conf->nomenclature->enabled && !empty($_REQUEST['fk_product']))
+		if ($conf->nomenclature->enabled && !empty($fk_product))
 		{
 			dol_include_once('/core/class/html.form.class.php');
 			dol_include_once('/asset/lib/asset.lib.php');
 			dol_include_once('/nomenclature/class/nomenclature.class.php');
 			
 			$doliForm = new Form($db);
-			echo $doliForm->selectarray('fk_nomenclature', TNomenclature::get($ATMdb, $_REQUEST['fk_product'], true));
+			echo $doliForm->selectarray('fk_nomenclature', TNomenclature::get($PDOdb, $fk_product, true));
 			
 			echo '<script type="text/javascript">
 				$(function() {
@@ -415,51 +388,53 @@ function _liste() {
 			
 		}
 		
-		echo '<a id="bt_createOf" class="butAction" href="fiche_of.php?action=new'.((isset($_REQUEST['fk_product'])) ? '&fk_product='.$_REQUEST['fk_product'] : '' ).'">'.$langs->trans('CreateOFAsset').'</a>';
+		echo '<a id="bt_createOf" class="butAction" href="fiche_of.php?action=new'.((!empty($fk_product)) ? '&fk_product='.$fk_product : '' ).'">'.$langs->trans('CreateOFAsset').'</a>';
 		echo '</div>';
 
 	}
 
-	$ATMdb->close();
-
+	$PDOdb->close();
 	llxFooter('');
 }
 
-function get_format_libelle_produit($fk_product = null) {
+function get_format_libelle_produit($fk_product = null) 
+{
 	global $db;
 
-	if (!empty($fk_product)) {
+	if (!empty($fk_product)) 
+	{
 		$product = new Product($db);
 		$product->fetch($fk_product);
 	
 		$product->ref.=' '.$product->label;
 	
 		return  $product->getNomUrl(1);
-	} else {
+	} 
+	else 
+	{
 		return 'Produit non défini.';
 	}
 }
 
-function get_format_libelle_societe($fk_soc) {
+function get_format_libelle_societe($fk_soc) 
+{
 	global $db;
 	
-    if($fk_soc>0) {
-	$societe = new Societe($db);
-	$societe->fetch($fk_soc);
-	$url = $societe->getNomUrl(1);
-	
-	return $url;
-	
+    if($fk_soc>0) 
+    {
+		$societe = new Societe($db);
+		$societe->fetch($fk_soc);
+		$url = $societe->getNomUrl(1);
+		
+		return $url;
     }
     
     return '';
 }
 
-function _printTicket()
+function _printTicket(&$PDOdb)
 {
 	global $db,$conf,$langs;
-
-	$PDOdb = new TPDOdb;
 	
 	$dirName = 'OF_TICKET('.date("Y_m_d").')';
 	$dir = DOL_DATA_ROOT.'/asset/'.$dirName.'/';
@@ -467,7 +442,7 @@ function _printTicket()
 	
 	$TPrintTicket = GETPOST('printTicket', 'array');
 	$TInfoEtiquette = _genInfoEtiquette($db, $PDOdb, $TPrintTicket);
-	//var_dump($TInfoEtiquette);exit;
+	
 	@mkdir($dir, 0777, true);
 	
 	if(defined('TEMPLATE_OF_ETIQUETTE')) $template = TEMPLATE_OF_ETIQUETTE;
@@ -522,7 +497,8 @@ function _genInfoEtiquette(&$db, &$PDOdb, &$TPrintTicket)
 							,'qty_to_print' => $qty
 							,'qty_to_make' => $assetOfLine->qty
 							,'label' => wordwrap(preg_replace('/\s\s+/', ' ', $product->label), 20, "<br />")
-							,'pos' => ceil($pos/8)						);
+							,'pos' => ceil($pos/8)						
+						);
 						
 						$pos++;
 					}
