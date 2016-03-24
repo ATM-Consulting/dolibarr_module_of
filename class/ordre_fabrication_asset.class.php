340928f168ae89d84aa08c463cdf23a3446c1482
root <root@vps235386.ovh.net>
2016-03-24 12:42:45 +0100

    FIX init var

--- class/ordre_fabrication_asset.class.php
+++ class/ordre_fabrication_asset.class.php
@@ -2349,10 +2349,11 @@ class TAssetWorkstationProduct extends TObjetStd {
 	
 	function __construct() {
 		$this->set_table(MAIN_DB_PREFIX.'asset_workstation_product');
-    	$this->TChamps = array(); 	  
+	    	$this->TChamps = array(); 	  
 		$this->add_champs('fk_product, fk_asset_workstation','type=entier;index;');
 		$this->add_champs('nb_hour_prepare,nb_hour_manufacture,nb_hour,rang','type=float;'); // nombre d'heure associé au poste de charge et au produit
 		
+		$this->_init_vars();
 		$this->start();
 		
 		$this->nb_hour=0;
@@ -2368,15 +2369,15 @@ class TAssetWorkstationOF extends TObjetStd{
 	
 	function __construct() {
 		$this->set_table(MAIN_DB_PREFIX.'asset_workstation_of');
-    	$this->TChamps = array(); 	  
+    		$this->TChamps = array(); 	  
 		$this->add_champs('fk_assetOf, fk_asset_workstation, fk_project_task','type=entier;index;');
 		$this->add_champs('nb_hour,nb_hour_real,nb_hour_prepare,rang','type=float;'); // nombre d'heure associé au poste de charge sur un OF
 		$this->add_champs('note_private',array('type'=>'text'));
 		// J'ai rajouté nb_hour_prepare dans cette table parce que quand on veut afficher le nombre d'heures de préparation pour un poste de travail sur l'odt of,
 		// celui ci peut être différent ligne par ligne si on a plusieurs fois un même poste de travail sur une nomenclature.
 		// as you wish buddy ! AA 
-		
-	    $this->start();
+		$this->_init_vars();
+	    	$this->start();
 		
 		$this->ws = new TAssetWorkstation; // TODO replace by TWorkstation
 		$this->users = array();
