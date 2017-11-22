2b25979416ef883b86f3f17c7cdcbb2613393b5f
Alexis ALGOUD <alexis@atm-consulting.fr>
2017-11-22 10:23:30 +0100

    Update modof.class.php

--- core/modules/modof.class.php
+++ core/modules/modof.class.php
@@ -302,7 +302,7 @@ class modof extends DolibarrModules
                     'url'=>'/of/liste_of.php?TListTBS[list_llx_assetOf][search][status]=NEEDOFFER',
                     'position'=>310+$r,
                     'enabled'=>'',            // Define condition to show or hide menu entry. Use '$conf->mymodule->enabled' if entry must be visible if module is enabled.
-                    'permsNEW_allow_to_link_product_to_workstation_for_prod'=>'$user->rights->of->of->lire',          // Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
+                    'perms'=>'$user->rights->of->of->lire',          // Use 'perms'=>'$user->rights->mymodule->level1->level2' if you want your menu with a permission rules
                     'target'=>'',
                     'user'=>2);             // 0=Menu for internal users, 1=external users, 2=both
         $r++;
