c56318422ec81f1129de6e501c07d38274809618
gauthier <gauthier.verdol@atm-consulting.fr>
2018-10-23 16:57:06 +0200

    FIX : tk8529 : erreur MAJ des quantités quand on clique sur le petit engrenage

--- script/interface.php
+++ script/interface.php
@@ -119,7 +119,7 @@ function _validerNomenclature(&$PDOdb, $id_assetOF, $fk_product, $fk_of_line, $f
 	$line->nomenclature_valide = 1;
 	
 	$of->addProductComposition($PDOdb, $fk_product, $line->qty, $fk_of_line, $fk_nomenclature);
-	$of->addWorkstation($PDOdb, $fk_product,$fk_nomenclature);
+	$of->addWorkstation($PDOdb, $fk_product,$fk_nomenclature,$line->qty);
 	
 	if ($of->fk_assetOf_parent) {
 		_validerOFLigneParent($PDOdb, $of, $fk_nomenclature, $line);
