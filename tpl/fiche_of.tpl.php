171093f17a78d48ea76f593e65486545fa663c8c
atm-lena <lena.papazian@atm-consulting.fr>
2021-09-15 11:47:30 +0200

    Add parameter id assetof

--- tpl/fiche_of.tpl.php
+++ tpl/fiche_of.tpl.php
@@ -384,7 +384,7 @@
 						<a onclick="if(!confirm('[view.langs.transnoentities(ReloadPriceFourn)]')) return false;" class="butActionDelete warning" href="[assetOf.url]?id=[assetOf.id]&action=reload_pmp">[view.langs.transnoentities(ReloadPrice)]</a>
 					[onshow;block=end]
 					[onshow;block=begin;when [stocktransfer.enable]==1]
-					&nbsp; &nbsp; <a href="[stocktransfer.url]?action=create" class="butAction">[view.langs.transnoentities(StockTransfer)]</a>
+					&nbsp; &nbsp; <a href="[stocktransfer.url]?action=create&id_of=[assetOf.id]" class="butAction">[view.langs.transnoentities(StockTransfer)]</a>
 					[onshow;block=end]
 					[onshow;block=begin;when [view.allow_delete_of_finish]!='1']
 						[onshow;block=begin;when [view.status]=='CLOSE']
